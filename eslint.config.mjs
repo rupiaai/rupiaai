7341598b1ef461e8a3e6f5bd9e658d5f5789b956
kasturirangan <kasturirangan20@gmail.com>
2025-04-26 01:54:57 +0530

    added rules for linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "react-hooks/exhaustive-deps": "off",
+      "@typescript-eslint/no-unsafe-function-type": "off",
+      "no-console": ["warn", { allow: ["warn"] }],
+    },
+  },
 ];
 
 export default eslintConfig;
